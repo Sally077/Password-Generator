5937ecac9287d82c1b117a799de6501656486ca6
Sally077 <sallyhedges77@gmail.com>
2023-01-14 14:26:32 +0000

    adding attempt at adding confirms when selected

--- script.js
+++ script.js
@@ -273,7 +273,7 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-// generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener('click', writePassword);
 
 
 
