1729ff52d6cf9e609d106d46c75359eedcae2bee
Sally077 <sallyhedges77@gmail.com>
2023-01-16 19:09:21 +0000

    final attempt for full solution before deadline

--- password.js
+++ password.js
@@ -1,101 +1,323 @@
-// Array of special characters to be included in password
+// // Array of special characters to be included in password
 
-var specialCharacters = [
-  '@',
-  '%',
-  '+',
-  '\\',
-  '/',
-  "'",
-  '!',
-  '#',
-  '$',
-  '^',
-  '?',
-  ':',
-  ',',
-  ')',
-  '(',
-  '}',
-  '{',
-  ']',
-  '[',
-  '~',
-  '-',
-  '_',
-  '.'
-];
-
-
-
-// Array of numeric characters to be included in password
-var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-
-// Array of lowercase characters to be included in password
- var lowerCasedCharacters = [
-  'a',
-  'b',
-  'c',
-  'd',
-  'e',
-  'f',
-  'g',
-  'h',
-  'i',
-  'j',
-  'k',
-  'l',
-  'm',
-  'n',
-  'o',
-  'p',
-  'q',
-  'r',
-  's',
-  't',
-  'u',
-  'v',
-  'w',
-  'x',
-  'y',
-  'z'
- ];
+// var specialCharacters = [
+//   '@',
+//   '%',
+//   '+',
+//   '\\',
+//   '/',
+//   "'",
+//   '!',
+//   '#',
+//   '$',
+//   '^',
+//   '?',
+//   ':',
+//   ',',
+//   ')',
+//   '(',
+//   '}',
+//   '{',
+//   ']',
+//   '[',
+//   '~',
+//   '-',
+//   '_',
+//   '.'
+// ];
 
 
 
-// Array of uppercase characters to be included in password
-var upperCasedCharacters = [
-  'A',
-  'B',
-  'C',
-  'D',
-  'E',
-  'F',
-  'G',
-  'H',
-  'I',
-  'J',
-  'K',
-  'L',
-  'M',
-  'N',
-  'O',
-  'P',
-  'Q',
-  'R',
-  'S',
-  'T',
-  'U',
-  'V',
-  'W',
-  'X',
-  'Y',
-  'Z'
-];
+// // Array of numeric characters to be included in password
+// var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-var len;
-var minCharacterLength = 10;
-var maxCharacterLength = 64;
+// // Array of lowercase characters to be included in password
+//  var lowerCasedCharacters = [
+//   'a',
+//   'b',
+//   'c',
+//   'd',
+//   'e',
+//   'f',
+//   'g',
+//   'h',
+//   'i',
+//   'j',
+//   'k',
+//   'l',
+//   'm',
+//   'n',
+//   'o',
+//   'p',
+//   'q',
+//   'r',
+//   's',
+//   't',
+//   'u',
+//   'v',
+//   'w',
+//   'x',
+//   'y',
+//   'z'
+//  ];
+
+
+
+// // Array of uppercase characters to be included in password
+// var upperCasedCharacters = [
+//   'A',
+//   'B',
+//   'C',
+//   'D',
+//   'E',
+//   'F',
+//   'G',
+//   'H',
+//   'I',
+//   'J',
+//   'K',
+//   'L',
+//   'M',
+//   'N',
+//   'O',
+//   'P',
+//   'Q',
+//   'R',
+//   'S',
+//   'T',
+//   'U',
+//   'V',
+//   'W',
+//   'X',
+//   'Y',
+//   'Z'
+// ];
+
+// var len;
+// var minCharacterLength = 10;
+// var maxCharacterLength = 64;
+// var pass = "";
+
+// var len = prompt("Enter a password length between 10 and 64 : ")
+
+// //ERROR cannot find a way to iterate if password does not meet length
+// // tried while which I feel would suit this code best, tried do and else if and for(tried len++ but unsure where to put it)
+// if (len >= minCharacterLength && len <= maxCharacterLength) { 
+//     len=alert("Characters meet condition length")
+  
+//  }else{ (len <= minCharacterLength && len >= maxCharacterLength)
+//   len=alert ("Characters do not meet length please enter a number between 10 and 64");
+//   var len = prompt("Enter a password length between 10 and 64 : ")
+
+// }
+   
+  
+
+
+// //step 3 set up if statements for confirms (now displays if true)
+
+
+// var char = confirm("Would you like to include special characters : ");
+// if (char === true)
+// pass += specialCharacters;
+// console.log(specialCharacters);
+// var lower = confirm("Would you like lowercase characters : ");
+// if (lower === true)
+// pass += lowerCasedCharacters;
+// console.log(lowerCasedCharacters);
+// var upper = confirm("Would you like uppercase characters :");
+// if (upper === true)
+// pass += upperCasedCharacters;
+// console.log(upperCasedCharacters);
+// var number = confirm("Would you like numbers included in your password :");
+// if (number === true)
+// pass += numericCharacters;
+// console.log(numericCharacters);
+
+
+
+// // console.log("Count Number of arrays if true",arrayCount.length);
+
+
+// console.log(len);
+// console.log(char);
+// console.log(lower);
+// console.log(upper);
+// console.log(number);
+
+
+// //step 3b if no confirms selected alert to user
+//  if (char === false && lower === false && upper === false && number === false) 
+//   alert("Please select at least one of the following : Special Characters, numbers, uppercase or lowercase ")
+
+
+
+// //step 1: generate random values from arrays(use mathfloor and math random)
+
+// function specialArray(){
+//      for(i = 0; i <= length; i++)
+//      return specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
+
+    
+
+// }
+
+// function numberArray(){
+//     for(i = 0; i <= length; i++)
+//     return numericCharacters[Math.floor(Math.random()*numericCharacters.length)];
+// }
+
+// function lowerArray(){
+//     for(i = 0; i <= length; i++)
+//     return lowerCasedCharacters[Math.floor(Math.random()*lowerCasedCharacters.length)];
+// }
+
+// function upperArray(){
+//     for(i = 0; i <= length; i++)
+//     return upperCasedCharacters[Math.floor(Math.random()*upperCasedCharacters.length)];
+// }
+
+
+
+// console.log(specialArray(specialCharacters));
+// console.log(numberArray(numericCharacters));
+// console.log(lowerArray(lowerCasedCharacters));
+// console.log(upperArray(upperCasedCharacters));
+
+
+// //step 2 concatenate arrays
+
+
+// const arrays = specialCharacters.concat(upperCasedCharacters, lowerCasedCharacters, numericCharacters);
+    
+
+// console.log(arrays);
+
+
+// // Get references to the #generate element
+// var generateBtn = document.querySelector('#generate');
+
+
+
+// // Write password to the #password input
+// function writePassword() {
+//     var password = generatePassword();
+//     var passwordText = document.querySelector('#password');
+  
+//     passwordText.value = password;
+// }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener('click', writePassword);
+
+// // step 4 include count to count arrays and check at least one statement is true ??
+// //initialise password variable
+
+
+// const randomfunc = {
+//     lower: lowerCasedCharacters,
+//     upper: upperCasedCharacters,
+//     number: numericCharacters,
+//     char: specialCharacters
+// };
+
+
+// //not working correctly needs fixing
+// function genPass(lower, upper, number, char, length) {
+//     var genpass = "" ;
+
+//     //attempt to filter arrays to select only a series of values to generate password
+//     const typesCount = [{lower} + {upper} + {number} + {char}].filter
+//     (item => Object.values(item)[0]);
+
+//     console.log("typesCount: ", typesCount);
+
+//     console.log("typesArr: ", typesArr);
+    
+// }
+// // attempt to generate password
+// function applypassword() {
+//     var password = "";
+
+//     for (var i = 0; i <= length; i++){
+    
+//         password += pass[Math.floor(Math.random() * pass.length)]
+//     }
+
+//     console.log("The password is : ",password);
+
+// }
+
+
+
+
+// // Test Data 
+// //attempt to count arrays, filter data from len then concat arrays
+
+// arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+// arr2 = ["!", "£", "$", "%", "^", "&", "*", "(", ")"]
+
+// function special() {
+//     for(i = 0; i <= length; i++)
+//     return arr2[Math.floor(Math.random() * arr2.length)]
+// }
+
+
+
+// function Num() {
+//     for(i = 0; i <= length; i++)
+//     return arr1[Math.floor(Math.random() * arr1.length)]
+// }
+// console.log(Num(arr1));
+// console.log(special(arr2));
+
+// var group = {
+//     arr1: Num,
+//     arr2: special
+// };
+
+// console.log(Object.entries(group)); //prints as an array but not details inside the array (perhaps try adding arrays inside?)
+// //console log to test and see if array will generate values up to the length entered in the prompt
+// console.log(arr1)
+// console.log(arr2)
+// // test to concat arrays then alter them
+
+// const catarray = arr1.concat(arr2);
+
+// console.log(catarray);
+
+
+
+
+// //types count attempt
+
+// array1 = arr1
+// array2 = arr2
+
+// typesCount = [array1] + [array2];
+
+//     console.log("typesCount: ", typesCount.length);
+//     //counts all the items inside the array but not the whole object
+
+//     // console.log("typesArr: ", typesArr);
+    
+    var len;
+    var minCharacterLength = 10;
+    var maxCharacterLength = 64;
+    var pass = "";
+
+
+var strings = {
+
+     specialCharacters: "!£$%^&*()@?:<>",
+     numericCharacters: "0123456789",
+     upperCasedCharacters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+     lowerCasedCharacters: "abcdefghijklmnopqrstuvwxyz",
+
+};
+
+
+
 var pass = "";
 
 var len = prompt("Enter a password length between 10 and 64 : ")
@@ -109,89 +331,35 @@ if (len >= minCharacterLength && len <= maxCharacterLength) {
   len=alert ("Characters do not meet length please enter a number between 10 and 64");
   var len = prompt("Enter a password length between 10 and 64 : ")
 
-}
-   
-  
-
-
-//step 3 set up if statements for confirms (now displays if true)
-
-
-var char = confirm("Would you like to include special characters : ");
-if (char === true)
-pass += specialCharacters;
+var specialCharacters = confirm("Would you like to include special characters : ");
+if (specialCharacters === true) 
+pass += strings.specialCharacters;
 console.log(specialCharacters);
+
 var lower = confirm("Would you like lowercase characters : ");
-if (lower === true)
-pass += lowerCasedCharacters;
+if (lowerCasedCharacters === true)
+pass += strings.lowerCasedCharacters;
 console.log(lowerCasedCharacters);
+
 var upper = confirm("Would you like uppercase characters :");
-if (upper === true)
-pass += upperCasedCharacters;
+if (upperCasedCharacters === true) {
+pass += strings.upperCasedCharacters;
 console.log(upperCasedCharacters);
-var number = confirm("Would you like numbers included in your password :");
-if (number === true)
-pass += numericCharacters;
+};
+var numericCharacters = confirm("Would you like numbers included in your password :");
+if (numericCharacters === true) {
+pass += strings.numericCharacters;
 console.log(numericCharacters);
+};
 
-
-
-// console.log("Count Number of arrays if true",arrayCount.length);
-
-
-console.log(len);
-console.log(char);
-console.log(lower);
-console.log(upper);
-console.log(number);
-
-
-//step 3b if no confirms selected alert to user
- if (char === false && lower === false && upper === false && number === false) 
-  alert("Please select at least one of the following : Special Characters, numbers, uppercase or lowercase ")
-
-
-
-//step 1: generate random values from arrays(use mathfloor and math random)
-
-function specialArray(){
-     for(i = 0; i <= length; i++)
-     return specialCharacters[Math.floor(Math.random()*specialCharacters.length)];
-
-    
-
+var password = "";
+for (i = 0; i < length; ++i) {
+    password += pass[Math.floor(Math.random() * pass.length)]
 }
-
-function numberArray(){
-    for(i = 0; i <= length; i++)
-    return numericCharacters[Math.floor(Math.random()*numericCharacters.length)];
-}
-
-function lowerArray(){
-    for(i = 0; i <= length; i++)
-    return lowerCasedCharacters[Math.floor(Math.random()*lowerCasedCharacters.length)];
-}
-
-function upperArray(){
-    for(i = 0; i <= length; i++)
-    return upperCasedCharacters[Math.floor(Math.random()*upperCasedCharacters.length)];
+console.log(password);
 }
 
 
-
-console.log(specialArray(specialCharacters));
-console.log(numberArray(numericCharacters));
-console.log(lowerArray(lowerCasedCharacters));
-console.log(upperArray(upperCasedCharacters));
-
-
-//step 2 concatenate arrays
-
-
-const arrays = specialCharacters.concat(upperCasedCharacters, lowerCasedCharacters, numericCharacters);
-    
-
-console.log(arrays);
 
 
 // Get references to the #generate element
@@ -209,98 +377,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
-
-// step 4 include count to count arrays and check at least one statement is true ??
-//initialise password variable
-
-
-const randomfunc = {
-    lower: lowerCasedCharacters,
-    upper: upperCasedCharacters,
-    number: numericCharacters,
-    char: specialCharacters
-};
-
-
-//not working correctly needs fixing
-function genPass(lower, upper, number, char, length) {
-    var genpass = "" ;
-
-    //attempt to filter arrays to select only a series of values to generate password
-    const typesCount = [{lower} + {upper} + {number} + {char}].filter
-    (item => Object.values(item)[0]);
-
-    console.log("typesCount: ", typesCount);
-
-    console.log("typesArr: ", typesArr);
-    
-}
-// attempt to generate password
-function applypassword() {
-    var password = "";
-
-    for (var i = 0; i <= length; i++){
-    
-        password += pass[Math.floor(Math.random() * pass.length)]
-    }
-
-    console.log("The password is : ",password);
-
-}
-
-
-
-
-// Test Data 
-//attempt to count arrays, filter data from len then concat arrays
-
-arr1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-arr2 = ["!", "£", "$", "%", "^", "&", "*", "(", ")"]
-
-function special() {
-    for(i = 0; i <= length; i++)
-    return arr2[Math.floor(Math.random() * arr2.length)]
-}
-
-
-
-function Num() {
-    for(i = 0; i <= length; i++)
-    return arr1[Math.floor(Math.random() * arr1.length)]
-}
-console.log(Num(arr1));
-console.log(special(arr2));
-
-var group = {
-    arr1: Num,
-    arr2: special
-};
-
-console.log(Object.entries(group)); //prints as an array but not details inside the array (perhaps try adding arrays inside?)
-//console log to test and see if array will generate values up to the length entered in the prompt
-console.log(arr1)
-console.log(arr2)
-// test to concat arrays then alter them
-
-const catarray = arr1.concat(arr2);
-
-console.log(catarray);
-
-
-
-
-//types count attempt
-
-array1 = arr1
-array2 = arr2
-
-typesCount = [array1] + [array2];
-
-    console.log("typesCount: ", typesCount.length);
-    //counts all the items inside the array but not the whole object
-
-    // console.log("typesArr: ", typesArr);
-    
-
-
 
